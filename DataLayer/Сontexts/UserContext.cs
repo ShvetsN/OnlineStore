a800244f17c59bfbdf56c56a1a36a4a90b76cb86
Nadiya Shvec <NadiyaYe.Shvec@livenau.net>
2018-05-14 21:54:46 +0300

    lkj

---
+++ DataLayer/Сontexts/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.Identity;
+
+namespace DataLayer.Contexts
+{
+    public class UserContext: IdentityDbContext<User>
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+    }
+}
---
+++ UnitOfWork/Interfaces/IUserUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitOfWork.Interfaces
+{
+    interface IUserUnitOfWork<T>: IDisposable where T:class
+    {
+        IUserRepository UsersProfile { get; }
+        UserManager<T> UserManager { get; }
+        RoleManager<T> RoleManager { get; }
+
+        Task SaveAsync();
+    }
+}
