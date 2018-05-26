30ac2384b351841f38b4b0126ca35d9300c1a2cd
Katttori(dima) <36365097+Katttori@users.noreply.github.com>
2018-05-26 22:48:35 +0300

    start of categoryController

---
+++ OnlineStore/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLogicLayer.Interfaces;
+using BusinessLogicLayer.Models;
+using OnlineStore.Models;
+using BusinessLogicLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace OnlineStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryManipulator _categoryManipulator;
+        IMapper _mapper;
+
+        public CategoryController(ICategoryManipulator categoryManipulator, IMapper mapper)
+        {
+            _categoryManipulator = categoryManipulator;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Create([FromBody] CategoryModel category)
+        {
+            var result = await _categoryManipulator.CreateCategory(_mapper.Map<CategoryBLL>(category));
+            if (result)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+
+        public async Task<IActionResult> Update([FromBody] CategoryModel category)
+        {
+            var result = await _categoryManipulator.UpdateCategory(_mapper.Map<CategoryBLL>(category));
+            if (result)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+    }
+}
