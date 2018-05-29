b797a262c746f4e734808c973c175f8620ee0d2d
Katttori(dima) <36365097+Katttori@users.noreply.github.com>
2018-05-29 17:36:10 +0300

    productbll

--- BusinessLogicLayer/Models/ProductBLL.cs
+++ BusinessLogicLayer/Models/ProductBLL.cs
@@ -6,12 +6,6 @@ namespace BusinessLogicLayer.Models
 {
     public class ProductBLL
     {
-        //public int Id { get; set; }
-        //public string Name { get; set; }
-        //public int CategoryId { get; set; }
-        //public double Price { get; set; }
-        //public string Photo { get; set; }
-        //public int Amount { get; set; }
 
         public int Id { get; set; }
 
